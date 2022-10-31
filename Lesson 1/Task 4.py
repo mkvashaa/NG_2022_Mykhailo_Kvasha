39a1cc07a7187a89541eacaacf52fc468a456ecb
mkkvashaa <infernofrt@gmail.com>
2022-10-31 19:22:37 +0300

    Without math

--- Lesson 1/Task 4.py
+++ Lesson 1/Task 4.py
@@ -1,12 +1,11 @@
-import math
 a = int(input("Input a:"))
 b = int(input("Input b:"))
 c = int(input("Input c:"))
 D = b**2-(4*a*c)
 if D>0:
-    x1 = (-b + math.sqrt(D))/(2*a)
-    x2 = (-b - math.sqrt(D))/(2*a)
-    print("x1=",x1, "x2=",x2)
+    x1 = (-b + (D**(1/2)))/(2*a)
+    x2 = (-b - (D**(1/2)))/(2*a)
+    print("x1=",x1,'\n' "x2=",x2)
 elif D == 0:
     x1 = (-b)/(2*a)
     print("x= ",x1)
